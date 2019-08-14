0a8cdde0e24bfdf2b601ac3fbe7b07cf4d839151
Gabriel Da Silva <50386282+FloppRap@users.noreply.github.com>
2019-08-14 09:09:24 -0300

    Create Ejercicio 2 by Flopp.cpp

---
+++ C++/C25/Ejercicio 2 by Flopp.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,i,aux=0;
+	int *pa, *pb;
+	
+	cout<<"Ingrese dos numeros. "<<endl;
+	cin>>a;
+	cout<<endl;
+	cin>>b;
+	
+	for (i=0;i<2;i++)
+	{
+		aux=b;
+		b=a;
+		a=aux;
+	}
+	
+	{
+	pa=&a;
+	pb=&b;
+	}
+	
+	cout<<endl;
+	cout<<"El valor primero ahora es: "<<*pb<<" En la posicion: "<<pb<<endl;
+	cout<<"El valor segundo ahora es: "<<*pa<<" En la posicion: "<<pa<<endl;
+	
+	return 0;
+}
